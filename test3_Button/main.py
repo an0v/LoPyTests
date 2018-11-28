247554fb8f334c8730112eec634e8279b1cd3d90
Andrey Novikov <nov.andrey@gmail.com>
2018-11-28 08:34:47 +0100

    test3: counter added

--- test3_Button/main.py
+++ test3_Button/main.py
@@ -10,24 +10,27 @@ led = Pin('G16',mode=Pin.OUT)
 # initialize GP17 in gpio mode and make it an input with the
 # pull-up enabled
 button = Pin('G17', mode=Pin.IN, pull=Pin.PULL_UP)
+counter = 0
 def callback(p):
-    print('pin change', p)
+    global counter
+    print('pin change. counter', counter)
     led.toggle()
+    counter = counter + 1
 
 button.callback(trigger= Pin.IRQ_FALLING, handler=callback)
 
 pycom.heartbeat(False)
-print('stating test 1.2')
+print('stating test 1.3')
 print(os.uname())
 #while True:
 for cycles in range(5): # stop after 5 cycles
     # send some data
-    print('diod: green')
+    print('diod: green ',cycles)
     pycom.rgbled(0x007f00) # green
     time.sleep(2)
-    print('diod: red')
+    print('diod: red ',cycles)
     pycom.rgbled(0x7f0000) # red
     time.sleep(2)
-    print('diod: blue')
+    print('diod: blue ',cycles)
     pycom.rgbled(0x00007f) # blue
     time.sleep(2)
